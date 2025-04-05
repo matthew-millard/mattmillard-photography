7f512da3ef0be02cd92d9b34cbae0c980cf97b27
matthew-millard <123032297+matthew-millard@users.noreply.github.com>
2025-04-05 18:06:51 -0400

    config: add basic, open graph and x metadata to index route

---
+++ app/metadata.ts
@@ -0,0 +1,6 @@
+export const domain = 'mattmillard.photography';
+export const title = 'Matt Millard Photography';
+export const siteName = 'Matt Millard Photography';
+export const author = 'Matt Millard';
+export const imageUrl = '';
+export const altText = '';
