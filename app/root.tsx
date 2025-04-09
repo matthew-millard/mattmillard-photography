1b8e4b6b23279c766e6f0062e9151ec2b22feff1
matthew-millard <123032297+matthew-millard@users.noreply.github.com>
2025-04-09 13:03:36 -0400

    config: setup cloudflare D1 database

--- app/root.tsx
+++ app/root.tsx
@@ -1,18 +1,37 @@
 import './styles/tailwind.css';
 import './styles/fonts.css';
 import type { ActionFunctionArgs, LinksFunction, LoaderFunctionArgs } from '@remix-run/cloudflare';
-import { Links, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
+import { data, Links, Meta, Outlet, Scripts, ScrollRestoration, useLoaderData } from '@remix-run/react';
 import { DesktopHeader, MobileHeader, Drawer, Footer } from './components/ui';
 import { updateThemeActionIntent } from './components/ui/theme-switch';
 import { getThemeFromCookie, updateTheme } from './.server/theme';
 import { useTheme } from './hooks';
 import { useState } from 'react';
 
+interface Customer {
+  CustomerId: string;
+  ContactName: string;
+  CompanyName: string;
+}
+
 export const links: LinksFunction = () => [];
 
-export async function loader({ request }: LoaderFunctionArgs) {
+export async function loader({ request, context }: LoaderFunctionArgs) {
   const theme = getThemeFromCookie(request);
-  return { theme };
+
+  const { env } = context.cloudflare;
+  const companyNameToQuery = 'Bs Beverages';
+  const ps = env.DB.prepare('SELECT * FROM Customers WHERE CompanyName=?').bind(companyNameToQuery);
+  const dbResponse = await ps.all();
+
+  if (!dbResponse.success) {
+    throw new Response('Yikes! Server error :(', {
+      status: 500,
+    });
+  }
+
+  const { results } = dbResponse;
+  return data({ theme, results: results as unknown as Customer[] });
 }
 
 export async function action({ request }: ActionFunctionArgs) {
@@ -27,6 +46,7 @@ export async function action({ request }: ActionFunctionArgs) {
   }
 }
 export function Layout({ children }: { children: React.ReactNode }) {
+  const { results: customers } = useLoaderData<typeof loader>();
   const [isDrawerOpen, setIsDrawerOpen] = useState<boolean>(false);
   const drawerState = { isDrawerOpen, setIsDrawerOpen };
   const theme = useTheme();
@@ -49,6 +69,13 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <div className="container min-h-screen flex flex-col">
           <MobileHeader {...drawerState} />
           <DesktopHeader />
+          {customers &&
+            customers.length > 0 &&
+            customers.map((customer: Customer) => (
+              <p key={customer.CustomerId}>
+                {customer.CustomerId}: {customer.ContactName} - {customer.CompanyName}
+              </p>
+            ))}
           <main className="flex-1">{children}</main>
           <Footer />
           <ScrollRestoration />
