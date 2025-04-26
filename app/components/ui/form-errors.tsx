7fda8edbbe0df08c2c7e111cebf9aca2c8574202
matthew-millard <123032297+matthew-millard@users.noreply.github.com>
2025-04-26 16:45:19 -0400

    style: add styling to admin login form

--- app/components/ui/form-errors.tsx
+++ app/components/ui/form-errors.tsx
@@ -3,12 +3,12 @@ export default function FormErrors({ errors }: { errors: string[] | undefined })
     <ul className="text-destructive text-sm">
       {errors && errors.length > 0 ? (
         errors.map(error => (
-          <li key={error} className="">
+          <li key={error} className="-mt-1">
             {error}
           </li>
         ))
       ) : (
-        <li>&nbsp;</li>
+        <li className="-mt-1">&nbsp;</li>
       )}
     </ul>
   );
