0988028b60139dd6a878ea9438b9f1eb9cb196bf
matthew-millard <123032297+matthew-millard@users.noreply.github.com>
2025-04-29 21:17:15 -0400

    feat: add image overlay

--- app/components/ui/image.tsx
+++ app/components/ui/image.tsx
@@ -1,12 +1,14 @@
 import { Link } from '@remix-run/react';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useInViewPort } from '~/hooks';
 import { cn } from '~/lib/utils';
 import { ImageRecord } from '~/routes/_index';
+import { P } from '../typography';
 
 export default function Image({ image }: { image: ImageRecord }) {
   const targetRef = useRef<HTMLImageElement>(null);
   const inViewPort = useInViewPort(targetRef, { threshold: 0.2 });
+  const [showOverlay, setShowOverlay] = useState<boolean>(false);
 
   //   Once the image has come into viewport, swap the lqip_url (blurred) for the high res url. Then remove the data-src attribute
   useEffect(() => {
@@ -20,7 +22,14 @@ export default function Image({ image }: { image: ImageRecord }) {
   }, [inViewPort]);
 
   return (
-    <Link to={`?pid=${image.id}`}>
+    <Link
+      to={`?pid=${image.id}`}
+      onMouseEnter={() => setShowOverlay(true)}
+      onMouseLeave={() => setShowOverlay(false)}
+      onFocus={() => setShowOverlay(true)}
+      onBlur={() => setShowOverlay(false)}
+      className="relative"
+    >
       <img
         ref={targetRef}
         data-src={image.url}
@@ -29,6 +38,15 @@ export default function Image({ image }: { image: ImageRecord }) {
         alt={image.alt_text ?? undefined}
         className={cn('w-full')}
       />
+      {showOverlay && <ImageOverlay alt_text={image.alt_text} />}
     </Link>
+  );
+}
+
+function ImageOverlay({ alt_text }: Pick<ImageRecord, 'alt_text'>) {
+  return (
+    <div className="absolute w-full h-full inset-0 bg-black/70 grid items-end overflow-y-scroll p-4">
+      <P>{alt_text}</P>
+    </div>
   );
 }
