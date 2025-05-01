5ebc522497dc7a154d58fab0f496da4475e8e06b
matthew-millard <123032297+matthew-millard@users.noreply.github.com>
2025-05-01 14:03:37 -0400

    chore: update metadat imageUrl and altText

--- app/metadata.ts
+++ app/metadata.ts
@@ -2,5 +2,5 @@ export const domain = 'mattmillard.photography';
 export const title = 'Matt Millard Photography';
 export const siteName = 'Matt Millard Photography';
 export const author = 'Matt Millard';
-export const imageUrl = '';
-export const altText = '';
+export const imageUrl = 'https://imagedelivery.net/AbeialkEo72QKV7n-TqWVA/2792b91d-7f3b-4562-d020-a87f60fb4c00/public';
+export const altText = 'Matt Millard';
