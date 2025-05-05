64a031636f9c8c44a43ddca0bb6b672c1dd1117d
matthew-millard <123032297+matthew-millard@users.noreply.github.com>
2025-05-04 21:29:25 -0400

    chore: change index route heading

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -60,10 +60,9 @@ export async function loader({ request, context }: LoaderFunctionArgs) {
     `
     SELECT * 
     FROM images 
-    ORDER BY RANDOM()
-    LIMIT 50
+    WHERE category = ?
   `
-  );
+  ).bind('collection');
   const dbResponse = await query.all<ImageRecord>();
 
   if (!dbResponse.success) {
@@ -92,7 +91,7 @@ export default function Index() {
     <div>
       <section>
         <Avatar src={imageUrl} alt={altText} className="md:hidden my-3 ml-2" />
-        <PageHeader title="Highlights" description="Food, drink and hospitality photographer based in Ottawa, Canada" />
+        <PageHeader title="Collection" description="Food, drink and hospitality photographer based in Ottawa, Canada" />
       </section>
       <section className="columns-2 md:columns-3 lg:columns-4 gap-4 md:gap-6 py-4">
         {images && images.length > 0 ? (
